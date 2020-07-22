5d103083130442d3bf337539b0ae4bce093b89f3
Kukhee Kim <khkim@slac.stanford.edu>
2020-07-21 19:10:16 -0700

    initial import for the driver source code

---
+++ pcavAsynApp/src/pcavAsyn.cpp
@@ -0,0 +1,434 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <cantProceed.h>
+#include <epicsTypes.h>
+#include <epicsTime.h>
+#include <epicsExit.h>
+#include <epicsThread.h>
+#include <epicsString.h>
+#include <epicsTimer.h>
+#include <epicsMutex.h>
+#include <epicsEvent.h>
+#include <epicsPrint.h>
+#include <ellLib.h>
+#include <iocsh.h>
+
+#include <yaml-cpp/yaml.h>
+#include <yamlLoader.h>
+
+#include <drvSup.h>
+#include <epicsExport.h>
+#include <registryFunction.h>
+
+#include <asynPortDriver.h>
+#include <asynOctetSyncIO.h>
+
+#include "pcavAsyn.h"
+
+
+static bool            keep_stay_in_loop = true;
+static epicsEventId    shutdownEvent;
+
+static ELLLIST *pDrvEllList = NULL;
+
+typedef struct {
+    ELLNODE         node;
+    char            *named_root;
+    char            *port;
+    char            *regPath;
+    char            *bsaStream;
+    pcavAsynDriver  *pcavAsyn;
+    void            *prv;
+} pDrvList_t;
+
+
+static void init_drvList(void)
+{
+    if(!pDrvEllList) {
+        pDrvEllList = (ELLLIST *) mallocMustSucceed(sizeof(ELLLIST), "pcavAsyn driver: init_drvList()");
+        ellInit(pDrvEllList);
+    }
+
+    return;
+} 
+
+static pDrvList_t *find_drvByPort(const char *port)
+{
+     init_drvList();
+     pDrvList_t *p = (pDrvList_t *) ellFirst(pDrvEllList);
+
+     while(p) {
+         if(p->port && strlen(p->port) && !strcmp(p->port, port)) break;
+         p = (pDrvList_t *) ellNext(&p->node);
+     }
+
+     return p;
+}
+
+static pDrvList_t *find_drvByNamedRoot(const char *named_root)
+{
+    init_drvList();
+    pDrvList_t *p = (pDrvList_t *) ellFirst(pDrvEllList);
+
+    while(p) {
+        if(p->named_root && strlen(p->named_root) && !strcmp(p->named_root, named_root)) break;
+        p = (pDrvList_t *) ellNext(&p->node);
+    }
+
+    return p;
+}
+
+
+pcavAsynDriver::pcavAsynDriver(void *pDrv, const char *portName, const char *pathString, const char *bsaStream, const char *named_root)
+    : asynPortDriver(portName,
+                     1, /* number of elements of this device */
+#if (ASYN_VERSION <<8 | ASYN_REVISION) < (4<<8 | 32)
+                     NUM_LLRFHLS_DET_PARAMS, /* number of asyn params of be cleared for each device */
+#endif  /* asyn version check, under 4.32 */
+                     asynInt32Mask | asynFloat64Mask | asynOctetMask | asynDrvUserMask | asynInt16ArrayMask | asynInt32ArrayMask | asynFloat64ArrayMask, /* Interface mask */
+                     asynInt32Mask | asynFloat64Mask | asynOctetMask | asynEnumMask    | asynInt16ArrayMask | asynInt32ArrayMask | asynFloat64ArrayMask,  /* Interrupt mask */
+                     1, /* asynFlags.  This driver does block and it is not multi-device, so flag is 1 */
+                     1, /* Autoconnect */
+                     0, /* Default priority */
+                     0) /* Default stack size*/
+{
+    Path p_root;
+    Path p_pcav;
+    port = epicsStrDup(portName);
+    path = epicsStrDup(pathString);
+    stream = (bsaStream && strlen(bsaStream))?epicsStrDup(bsaStream): NULL;
+    this->pDrv = pDrv;
+
+    try {
+        p_root = (named_root && strlen(named_root))? cpswGetNamedRoot(named_root): cpswGetRoot();
+        p_pcav = p_root->findByName(pathString);
+    } catch (CPSWError &e) {
+        fprintf(stderr, "CPSW Error: %s, file %s, line %d\n", e.getInfo().c_str(), __FILE__, __LINE__);
+        throw e;
+    }
+
+    _pcav = IpcavFw::create(p_pcav);
+
+
+    ParameterSetup();
+
+}
+
+
+pcavAsynDriver::~pcavAsynDriver() {}
+
+
+asynStatus pcavAsynDriver::writeInt32(asynUser *pasynUser, epicsInt32 value)
+{
+    int        function      = pasynUser->reason;
+    asynStatus status        = asynSuccess;
+    const char *functionName = "writeInt32";
+
+    status = (asynStatus) setIntegerParam(function, value);
+
+    if(function == p_rfRefSel)                 _pcav->setRefSel((uint32_t) value);
+    else if (function == p_rfRefWindowStart)   _pcav->setRefWindowStart((uint32_t) value);
+    else if (function == p_rfRefWindowEnd)     _pcav->setRefWindowEnd((uint32_t) value);
+    else
+
+    for(int i = 0; i < NUM_CAV; i++) {
+        for(int j = 0; j < NUM_PROBE; j++) {
+            if(function == p_cavChanSel[i][j]) {
+                _pcav->setChanSel(i, j, (uint32_t) value); goto _escape;
+            }
+            if(function == p_cavWindowStart[i][j]) {
+                _pcav->setWindowStart(i, j, (uint32_t) value); goto _escape;
+            }
+            if(function == p_cavWindowEnd[i][j]) {
+                _pcav->setWindowEnd(i, j, (uint32_t) value); goto _escape;
+            }
+            if(function == p_cavCalibCoeff[i][j]) {
+                _pcav->setCalibCoeff(i, j, (uint32_t) value); goto _escape;
+            }
+        }
+    }
+    
+    _escape:
+
+    return status;
+}
+
+asynStatus pcavAsynDriver::writeFloat64(asynUser *pasynUser, epicsFloat64 value)
+{
+    int        function      = pasynUser->reason;
+    asynStatus status        = asynSuccess;
+    const char *functionName = "writeFloat64";
+
+    status = (asynStatus) setDoubleParam(function, value);
+
+    for(int i = 0; i < NUM_CAV; i++) {
+        if(function == p_cavNCOPhaseAdj[i]) {
+            _pcav->setNCO(i, value); goto _escape;
+        }
+    }
+
+    _escape:
+    return status;
+}
+
+void pcavAsynDriver::report(int interest)
+{
+}
+
+void pcavAsynDriver::poll(void)
+{
+    monitor();
+    callParamCallbacks();
+}
+
+
+void pcavAsynDriver::ParameterSetup(void)
+{
+    char param_name[80];
+    char raw_param_name[83];
+
+    // rf reference monitoring
+    sprintf(param_name,     RFREF_AMPL_STR);            createParam(param_name,     asynParamFloat64, &(p_rfRefAmpl.val));
+    sprintf(raw_param_name, RAW_PARAM_STR, param_name); createParam(raw_param_name, asynParamInt32,   &(p_rfRefAmpl.raw));
+    sprintf(param_name,     RFREF_PHASE_STR);           createParam(param_name,     asynParamFloat64, &(p_rfRefPhase.val));
+    sprintf(raw_param_name, RAW_PARAM_STR, param_name); createParam(raw_param_name, asynParamInt32,   &(p_rfRefPhase.raw));
+    sprintf(param_name,     RFREF_I_STR);               createParam(param_name,     asynParamFloat64, &(p_rfRefI.val));
+    sprintf(raw_param_name, RAW_PARAM_STR, param_name); createParam(raw_param_name, asynParamInt32,   &(p_rfRefI.raw));
+    sprintf(param_name,     RFREF_Q_STR);               createParam(param_name,     asynParamFloat64, &(p_rfRefQ.val));
+    sprintf(raw_param_name, RAW_PARAM_STR, param_name); createParam(raw_param_name, asynParamInt32,   &(p_rfRefQ.raw));
+    // rf reference control
+    sprintf(param_name,     RFREF_SEL_STR);             createParam(param_name,     asynParamInt32,   &p_rfRefSel);
+    sprintf(param_name,     RFREF_WINDOW_START_STR);    createParam(param_name,     asynParamInt32,   &p_rfRefWindowStart);
+    sprintf(param_name,     RFREF_WINDOW_END_STR);      createParam(param_name,     asynParamInt32,   &p_rfRefWindowEnd);
+
+
+    for(int cav = 0; cav < NUM_CAV; cav++) {
+        for(int probe = 0; probe < NUM_PROBE; probe++) {
+            // cavitiy monitor
+            sprintf(param_name,     CAV_IF_AMPL_STR,      cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavIfAmpl[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavIfAmpl[cav][probe].raw));
+            sprintf(param_name,     CAV_IF_PHASE_STR,     cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavIfPhase[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavIfPhase[cav][probe].raw));
+            sprintf(param_name,     CAV_IF_I_STR,         cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavIfI[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavIfI[cav][probe].raw));
+            sprintf(param_name,     CAV_IF_Q_STR,         cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavIfQ[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavIfQ[cav][probe].raw));
+            sprintf(param_name,     CAV_DCREAL_STR,       cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavDCReal[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavDCReal[cav][probe].raw));
+            sprintf(param_name,     CAV_DCIMAGE_STR,      cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavDCImage[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavDCImage[cav][probe].raw));
+            sprintf(param_name,     CAV_DCFREQ_STR,       cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavDCFreq[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavDCFreq[cav][probe].raw));
+            sprintf(param_name,     CAV_INTEG_I_STR,      cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavIntegI[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavIntegI[cav][probe].raw));
+            sprintf(param_name,     CAV_INTEG_Q_STR,      cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavIntegQ[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavIntegQ[cav][probe].raw));
+            sprintf(param_name,     CAV_OUT_PHASE_STR,    cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavOutPhase[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavOutPhase[cav][probe].raw));
+            sprintf(param_name,     CAV_OUT_AMPL_STR,     cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavOutAmpl[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavOutAmpl[cav][probe].raw));
+            sprintf(param_name,     CAV_COMP_I_STR,       cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavCompI[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavCompI[cav][probe].raw));
+            sprintf(param_name,     CAV_COMP_Q_STR,       cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavCompQ[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavCompQ[cav][probe].raw));
+            sprintf(param_name,     CAV_COMP_PHASE_STR,   cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavCompPhase[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavCompPhase[cav][probe].raw));
+            sprintf(param_name,     CAV_IF_WF_STR,        cav, probe); createParam(param_name,     asynParamFloat64, &(p_cavIfWf[cav][probe].val));
+            sprintf(raw_param_name, RAW_PARAM_STR,        param_name); createParam(raw_param_name, asynParamInt32,   &(p_cavIfWf[cav][probe].raw));
+            // cavity control, per probe
+            sprintf(param_name,     CAV_CHANSEL_STR,      cav, probe); createParam(param_name,     asynParamInt32,   &(p_cavChanSel[cav][probe]));
+            sprintf(param_name,     CAV_WINDOW_START_STR, cav, probe); createParam(param_name,     asynParamInt32,   &(p_cavWindowStart[cav][probe]));
+            sprintf(param_name,     CAV_WINDOW_END_STR,   cav, probe); createParam(param_name,     asynParamInt32,   &(p_cavWindowEnd[cav][probe]));
+        }
+        // cavity control, per cavity
+        sprintf(param_name, CAV_NCO_PHASE_ADJ_STR, cav); createParam(param_name, asynParamInt32, &(p_cavNCOPhaseAdj[cav]));
+
+    }
+    
+}
+
+
+void pcavAsynDriver::monitor(void)
+{
+    double      val;
+    int32_t     raw;
+
+    val = _pcav->getRefAmpl(&raw);   setDoubleParam(p_rfRefAmpl.val,  val); setIntegerParam(p_rfRefAmpl.raw,  raw);
+    val = _pcav->getRefPhase(&raw);  setDoubleParam(p_rfRefPhase.val, val); setIntegerParam(p_rfRefPhase.raw, raw);  
+    val = _pcav->getRefI(&raw);      setDoubleParam(p_rfRefI.val,     val); setIntegerParam(p_rfRefI.raw,     raw);
+    val = _pcav->getRefQ(&raw);      setDoubleParam(p_rfRefQ.val,     val); setIntegerParam(p_rfRefQ.raw,     raw);
+
+    for(int i = 0; i < NUM_CAV; i++) {
+        for(int j = 0; j < NUM_PROBE; j++) {
+            val = _pcav->getIfAmpl(i, j, &raw);     setDoubleParam(p_cavIfAmpl[i][j].val,    val); setIntegerParam(p_cavIfAmpl[i][j].raw,    raw);
+            val = _pcav->getIfPhase(i, j, &raw);    setDoubleParam(p_cavIfPhase[i][j].val,   val); setIntegerParam(p_cavIfPhase[i][j].raw,   raw);
+            val = _pcav->getIfI(i, j, &raw);        setDoubleParam(p_cavIfI[i][j].val,       val); setIntegerParam(p_cavIfI[i][j].raw,       raw);
+            val = _pcav->getIfQ(i, j, &raw);        setDoubleParam(p_cavIfQ[i][j].val,       val); setIntegerParam(p_cavIfQ[i][j].raw,       raw);
+            val = _pcav->getDCReal(i, j, &raw);     setDoubleParam(p_cavDCReal[i][j].val,    val); setIntegerParam(p_cavDCReal[i][j].raw,    raw);
+            val = _pcav->getDCImage(i, j, &raw);    setDoubleParam(p_cavDCImage[i][j].val,   val); setIntegerParam(p_cavDCImage[i][j].raw,   raw);
+            val = _pcav->getDCFreq(i, j, &raw);     setDoubleParam(p_cavDCFreq[i][j].val,    val); setIntegerParam(p_cavDCFreq[i][j].raw,    raw);
+            val = _pcav->getIntegI(i, j, &raw);     setDoubleParam(p_cavIntegI[i][j].val,    val); setIntegerParam(p_cavIntegI[i][j].raw,    raw);
+            val = _pcav->getIntegQ(i, j, &raw);     setDoubleParam(p_cavIntegQ[i][j].val,    val); setIntegerParam(p_cavIntegQ[i][j].raw,    raw);
+            val = _pcav->getOutPhase(i, j, &raw);   setDoubleParam(p_cavOutPhase[i][j].val,  val); setIntegerParam(p_cavOutPhase[i][j].raw,  raw);
+            val = _pcav->getOutAmpl(i, j, &raw);    setDoubleParam(p_cavOutAmpl[i][j].val,   val); setIntegerParam(p_cavOutAmpl[i][j].raw,   raw);
+            val = _pcav->getCompI(i, j, &raw);      setDoubleParam(p_cavCompI[i][j].val,     val); setIntegerParam(p_cavCompI[i][j].raw,     raw);
+            val = _pcav->getCompQ(i, j, &raw);      setDoubleParam(p_cavCompQ[i][j].val,     val); setIntegerParam(p_cavCompQ[i][j].raw,     raw);
+            val = _pcav->getCompPhase(i, j, &raw);  setDoubleParam(p_cavCompPhase[i][j].val, val); setIntegerParam(p_cavCompPhase[i][j].raw, raw);
+        }
+    }
+
+
+}
+
+
+extern "C" {
+
+int pcavAsynDriverConfigure(const char *portName, const char *regPathString, const char *bsaStream, const char *named_root)
+{
+    init_drvList();
+
+    pDrvList_t *p = find_drvByPort(portName);
+    if(p) {
+        printf("pcavAsynDriver found the port name (%s) has been used.\n", portName);
+        return 0;
+    }
+
+    p = (pDrvList_t *) mallocMustSucceed(sizeof(pDrvList_t), "pcavAsynDriver: pcavAsynDriverConfigure()");
+    p->named_root = (named_root && strlen(named_root))?epicsStrDup(named_root):cpswGetRootName();
+    p->port       = epicsStrDup(portName);
+    p->regPath    = epicsStrDup(regPathString);
+    p->bsaStream  = (bsaStream && strlen(bsaStream))? epicsStrDup(bsaStream): NULL;
+    p->pcavAsyn   = new pcavAsynDriver((void *) p, (const char *) p->port, (const char *) p->regPath, (const char *) p->bsaStream, (const char *) p->named_root);
+    p->prv        = NULL;
+
+    ellAdd(pDrvEllList, &p->node);
+
+    return 0;
+}
+
+
+// prepare ioc command for driver initialization
+static const iocshArg initArg0 = {"port name",     iocshArgString};
+static const iocshArg initArg1 = {"register path", iocshArgString};
+static const iocshArg initArg2 = {"bsa stream",    iocshArgString};
+static const iocshArg initArg3 = {"named_root",    iocshArgString};
+static const iocshArg * const initArgs[] = { &initArg0,
+                                             &initArg1,
+                                             &initArg2,
+                                             &initArg3 };
+static const iocshFuncDef initFuncDef = {"pcavAsynDriverConfigure", 4, initArgs};
+static void  initCallFunc(const iocshArgBuf *args)
+{
+    pcavAsynDriverConfigure(args[0].sval,
+                            args[1].sval,
+                            (args[2].sval && strlen(args[2].sval))? args[2].sval: NULL,
+                            (args[3].sval && strlen(args[3].sval))? args[3].sval: NULL);
+}
+
+static void pcavAsynDriverRegister(void)
+{
+    iocshRegister(&initFuncDef, initCallFunc);
+}
+
+epicsExportRegistrar(pcavAsynDriverRegister);
+
+
+
+static int pcavAsynDriverPoll(void)
+{
+    while(keep_stay_in_loop) {
+        pDrvList_t *p = (pDrvList_t *) ellFirst(pDrvEllList);
+        while(p) {
+            if(p->pcavAsyn) p->pcavAsyn->poll();
+            p = (pDrvList_t *) ellNext(&p->node);
+        }
+        epicsThreadSleep(1.);
+    }
+
+    epicsEventSignal(shutdownEvent);
+
+    return 0;
+    
+}
+
+static void stopPollingThread(void *p)
+{
+    keep_stay_in_loop = false;
+    epicsEventWait(shutdownEvent);
+    epicsPrintf("pcavAsynDriver: stop polling thread (%s)\n", (char*) p);
+}
+
+
+// EPICS driver support for pcavAsynDriver
+static int pcavAsynDriverReport(int interest);
+static int pcavAsynDriverInitialize(void);
+
+static struct drvet pcavAsynDriver = {
+    2,
+    (DRVSUPFUN) pcavAsynDriverReport,
+    (DRVSUPFUN) pcavAsynDriverInitialize
+};   
+
+epicsExportAddress(drvet, pcavAsynDriver);
+
+static int pcavAsynDriverReport(int interest)
+{
+    init_drvList();
+    printf("Total %d of pcavAsynDriver instance(s) is (are) registered\n", ellCount(pDrvEllList));
+
+    pDrvList_t *p = (pDrvList_t *) ellFirst(pDrvEllList);
+    while(p) {
+        printf("\tnamed root    : %s\n", p->named_root);
+        printf("\tport name     : %s\n", p->port);
+        printf("\tregister path : %s\n", p->regPath);
+        printf("\tbsa stream    : %s\n", p->bsaStream);
+        printf("\tpcavAsyn      : %p\n", p->pcavAsyn);
+        if(p->pcavAsyn) p->pcavAsyn->report(interest);
+        p = (pDrvList_t *) ellNext(&p->node);
+    }
+
+    return 0;
+}
+
+static int pcavAsynDriverInitialize(void)
+{
+    init_drvList();
+
+    if(!pDrvEllList) {
+        printf("pcavAsynDriver never been configured\n");
+        return 0;
+    }
+
+    keep_stay_in_loop = true;
+    shutdownEvent     = epicsEventMustCreate(epicsEventEmpty);
+    const char *name  = "pcavAsynDriver";
+
+    epicsThreadCreate(name, epicsThreadPriorityMedium,
+                      epicsThreadGetStackSize(epicsThreadStackMedium),
+                      (EPICSTHREADFUNC) pcavAsynDriverPoll, 0);
+
+    epicsAtExit3((epicsExitFunc) stopPollingThread, (void*) epicsStrDup(name), epicsStrDup(name));
+
+    return 0;
+}
+
+
+
+} /* extern C */
+
+
+
+
---
+++ pcavAsynApp/src/pcavAsyn.h
@@ -0,0 +1,144 @@
+#ifndef _PCAVASYN_H
+#define _PCAVASYN_H
+
+#include <asynPortDriver.h>
+#include <epicsEvent.h>
+#include <epicsTypes.h>
+#include <epicsTime.h>
+
+#include <cpsw_api_user.h>
+#include <pcavFw.h>
+
+#include <vector>
+#include <string>
+#include <dlfcn.h>
+
+#include <stdio.h>
+#include <sstream>
+#include <fstream>
+
+
+#define NUM_CAV         2       // number of cavities
+#define NUM_PROBE       2       // number of probes per cavity 
+
+typedef struct {
+    int raw;
+    int val;
+} pcavMon;
+
+
+class pcavAsynDriver
+    :asynPortDriver {
+    public:
+        pcavAsynDriver(void *pDrv, const char *portName, const char *pathString, const char *bsaStream, const char *named_root = NULL);
+        ~pcavAsynDriver();
+        asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
+        asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
+        void report(int interest);
+        void poll(void);
+
+    private:
+        void    *pDrv;
+        char    *port;
+        char    *path;
+        char    *stream;
+        pcavFw  _pcav;
+
+        void ParameterSetup(void);
+        void monitor(void);
+
+
+    protected:
+#if (ASYN_VERSION <<8 | ASYN_REVISION) < (4<<8 | 32)
+        int firstPcavParam;
+#define FIRST_PCAV_PARAM   firstPcavsParam
+#endif /* ASYN VERSION CHECK under 4.32 */
+
+        // rf reference 
+        pcavMon p_rfRefAmpl;
+        pcavMon p_rfRefPhase;
+        pcavMon p_rfRefI;
+        pcavMon p_rfRefQ;
+        int     p_rfRefSel;
+        int     p_rfRefWindowStart;
+        int     p_rfRefWindowEnd;
+
+        // 2 cavities, 2 probes
+        pcavMon p_cavIfAmpl[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavIfPhase[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavIfI[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavIfQ[NUM_CAV][NUM_PROBE];
+        int     p_cavChanSel[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavDCReal[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavDCImage[NUM_CAV][NUM_PROBE];
+        int     p_cavWindowStart[NUM_CAV][NUM_PROBE];
+        int     p_cavWindowEnd[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavDCFreq[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavIntegI[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavIntegQ[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavOutPhase[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavOutAmpl[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavCompI[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavCompQ[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavCompPhase[NUM_CAV][NUM_PROBE];
+        pcavMon p_cavIfWf[NUM_CAV][NUM_PROBE];       // TBD  cavIfWf
+        int     p_cavCalibCoeff[NUM_CAV][NUM_PROBE]; // TBD  cavCalibCoeff
+
+        // 2 cavities
+        int     p_cavNCOPhaseAdj[NUM_CAV];
+
+#if (ASYN_VERSION <<8 | ASYN_REVISION) < (4<<8 | 32)
+        int lastPcavParam;
+#define LAST_PCAV_PARAM   lastPcavParam
+#endif /* ASYN VERSION CHECK under 4.32 */
+
+
+};
+
+
+#if (ASYN_VERSION <<8 | ASYN_REVISION) < (4<<8 | 32)
+#define NUM_PCAV_DET_PARAMS ((int)(&LAST_PCAV_PARAM - &FIRST_PCAV_PARAM - 1))
+#endif /* ASYN VERSION CHECK under 4.32 */
+
+
+
+/* rf reference */
+
+#define RFREF_AMPL_STR            "rfRefAmpl"
+#define RFREF_PHASE_STR           "rfRefPhase"
+#define RFREF_I_STR               "rfRefI"
+#define RFREF_Q_STR               "rfRefQ"
+#define RFREF_SEL_STR             "rfRefSel"
+#define RFREF_WINDOW_START_STR    "rfRefWindowStart"
+#define RFREF_WINDOW_END_STR      "rfRefWindowEnd"
+
+
+/*2 cavties and 2 probes */
+#define CAV_IF_AMPL_STR           "cav%dP%dIfAmpl"
+#define CAV_IF_PHASE_STR          "cav%dP%dIfPhase"
+#define CAV_IF_I_STR              "cav%dP%dIfI"
+#define CAV_IF_Q_STR              "cav%dP%dIfQ"
+#define CAV_CHANSEL_STR           "cav%dP%dChanSel"
+#define CAV_DCREAL_STR            "cav%dP%dDCReal"
+#define CAV_DCIMAGE_STR           "cav%dP%dDCImage"
+#define CAV_WINDOW_START_STR      "cav%dP%dWindowStart"
+#define CAV_WINDOW_END_STR        "cav%dP%dWindowEnd"
+#define CAV_DCFREQ_STR            "cav%dP%dDCFreq"
+#define CAV_INTEG_I_STR           "cav%dP%dIntegI"
+#define CAV_INTEG_Q_STR           "cav%dP%dIntegQ"
+#define CAV_OUT_PHASE_STR         "cav%dP%dOutPhase"
+#define CAV_OUT_AMPL_STR          "cav%dP%dOutAmpl"
+#define CAV_COMP_I_STR            "cav%dP%dCompI"
+#define CAV_COMP_Q_STR            "cav%dP%dCompQ"
+#define CAV_COMP_PHASE_STR        "cav%dP%dCompPhase"
+#define CAV_IF_WF_STR             "cav%dP%dIfWf"         // space holder TBD
+#define CAV_CALIB_COEFF_STR       "cav%dP%dCalibCoeff"   // space holder TBD
+
+/* 2 cavities */
+#define CAV_NCO_PHASE_ADJ_STR     "cav%dNCOPhaseAdj"
+
+#define RAW_PARAM_STR             "%sRaw"
+
+
+
+#endif /* _PCAVASYN_H */
